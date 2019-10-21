9a3179fa55c9e160229c77051b02751692633026
AGonzalez5195 <AnthonyGonzalez@pursuit.org>
2019-10-21 15:47:28 -0400

    eat ass

--- NYTimesGroupProj/NYTimesGroupProj/Controllers/FavoritesViewController.swift
+++ NYTimesGroupProj/NYTimesGroupProj/Controllers/FavoritesViewController.swift
@@ -57,7 +57,7 @@ extension FavoritesViewController: UICollectionViewDataSource {
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     let favsCell = favsCollectionView.dequeueReusableCell(withReuseIdentifier: "favoritesCell", for: indexPath) as! FavoritesCollectionViewCell
     
-    //TODO: - Load data for favorites
+  
     return favsCell
   }
 }
@@ -68,21 +68,10 @@ extension FavoritesViewController: UICollectionViewDelegateFlowLayout {
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
     return CGSize(width: 350, height: 350)
   }
-
-    func collectionView() {
         
-    }
-    
-    
-    
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = #colorLiteral(red: 1, green: 0.9799128175, blue: 0.8817918897, alpha: 1)
     }
-    
-    
-    
-    
 }
